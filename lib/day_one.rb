8505353f87e58d11fb0edfcd91bd6c0435f93e3e
RSijelmass <R.Sijelmass@gmail.com>
2017-05-08 16:55:42 +0100

    Completes Challenge 2

--- lib/day_one.rb
+++ lib/day_one.rb
@@ -1,3 +1,7 @@
 def adds_one(array)
   array.map {|x| x+1}
 end
+
+def sort_array(array)
+  array.sort
+end
--- spec/day_one_spec.rb
+++ spec/day_one_spec.rb
@@ -4,4 +4,7 @@ describe 'day_one' do
   it 'adds 1 to every value' do
     expect(adds_one([1,2,3,4,5])).to eq [2,3,4,5,6]
   end
+  it 'sorts the array' do
+    expect(sort_array([1,3,5,4,2])).to eq [1,2,3,4,5]
+  end
 end
